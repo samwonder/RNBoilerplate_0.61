6452cbb23ee7ced2c1719adb3020df44e939a2f5
Praveen Deshmukh <praveen.deshmukh88@gmail.com>
2020-03-13 13:45:51 +0530

    Setup Initial boilerplate wit navigation version 5

---
+++ app/navigator/drawerStack.js
@@ -0,0 +1,22 @@
+// import {Dimensions} from 'react-native';
+// import {createDrawerNavigator} from 'react-navigation-drawer';
+// import Dashboard from '../screens/home';
+// import SideMenu from '../screens/SideMenu';
+// // import {screenNames} from '../utils/constant';
+
+// const DrawerStack = createDrawerNavigator(
+//   {
+//     Dashboard: {
+//       screen: Dashboard,
+//       key: 'HomeScreen',
+//     },
+//   },
+//   {
+//     contentComponent: SideMenu,
+//     drawerWidth: Dimensions.get('window').width - 100,
+//     drawerPosition: 'left',
+//     drawerType: 'front',
+//   },
+// );
+
+// export default DrawerStack;
